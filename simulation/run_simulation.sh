ec42ebe16f6835371f7c59ebd40867f19e5e2753
Oliver Contier <o.contier@gmail.com>
2017-07-16 21:08:24 -0400

    quick fix, forgot to add input/output path to run script

--- simulation/run_simulation.sh
+++ simulation/run_simulation.sh
@@ -4,4 +4,7 @@ echo "Sourcing FSL"
 source /etc/fsl/fsl.sh
 
 echo "Running script"
-python famface_simulation_main.py $1
+python famface_simulation_main.py $1 \
+    /data/famface/openfmri/oli/simulation/data_oli \
+    /data/famface/openfmri/scripts/notebooks/rois_manual_r5_20170222_nooverlap.nii.gz \
+    /data/famface/openfmri/oli/simulation/mcfiles
