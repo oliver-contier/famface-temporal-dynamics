d6cb2fb86470d2e580f9245f0abe13cd70b69a85
Oliver Contier <o.contier@gmail.com>
2017-07-24 11:56:48 -0400

    use existing transformation matrix from actual results to project mni mask into subject space

--- simulation/famface_simulation_main.py
+++ simulation/famface_simulation_main.py
@@ -6,7 +6,8 @@ Run simulation over our data
 
 if __name__ == "__main__":
 
-    import os
+    from os.path import join
+    import copy
     from famface_simulation_functions import *
     from mvpa2.datasets.mri import fmri_dataset, map2nifti
     import sys
@@ -16,33 +17,54 @@ if __name__ == "__main__":
     maskpath = sys.argv[3]
     workdirbase = sys.argv[4]
 
-    # data_basedidr = '/data/famface/openfmri/oli/simulation/data'
+    # template for directory name containing subjects data in the working directory
+    subdir_template = '_model_id_1_subject_id_%s_task_id_1' % sub
+
+    # path to anatomical image
+    anat = join('/data/famface/openfmri/oli/results/extract_betas/l1_workdir_betas/',
+                'registration', subdir_template, 'stripper',
+                'highres001_brain.nii.gz')
+
+    # path to hd5 file and affine transformation matrix
+    mni2anat_hd5 = join('/data/famface/openfmri/oli/results/extract_betas/l1_workdir_betas/',
+                        'registration', subdir_template,
+                        'antsRegister', 'output_InverseComposite.h5')
+    affine_matrix = join('/data/famface/openfmri/oli/results/extract_betas/l1_workdir_betas/',
+                         'registration', subdir_template,
+                         'mean2anatbbr', 'median_flirt.mat')
+
     # maskpath = '/data/famface/openfmri/scripts/notebooks/rois_manual_r5_20170222_nooverlap.nii.gz'
 
+    # initial amplitudes for fam and unfam (first run). will be changed for each new run.
     amplitudes = [8, 1]
+
+    # get runs
     runs_unsorted = os.listdir(os.path.join(data_basedir, sub, 'model/model001/onsets'))
     runs = [run for run in sorted(runs_unsorted)]
 
     for run in runs:
 
+        # create run specific working directory
         workdir = os.path.join(workdirbase, sub, run)
         if not os.path.exists(workdir):
             os.makedirs(workdir)
 
-        mni_run_dir = run.replace('task001_', '')
+        # path to bold file
+        boldfile = join(data_basedir, sub, 'BOLD', run, 'bold.nii.gz')
 
-        # TODO: change input bold (subject space)
-        noise = simulate_run(os.path.join(data_basedir, sub, 'BOLD', run, 'bold.nii.gz'), workdir)
+        # simulate noise image
+        noise = simulate_run(boldfile, workdir)
 
         # get onsets
         spec = get_onsets_famface(
             os.path.join(data_basedir, sub, 'model/model001/onsets', run),
             amplitudes)
+
+        # increment amplitude for unfam
         amplitudes[1] += 0.5
 
-        # create second specification for contrast that does not increase and give it another roi value
-        import copy
-
+        # create second specification for contrast that does not increase
+        # in different roi
         straightspec = copy.deepcopy(spec)
         straightspec[0]['amplitude'] = 8
         straightspec[1]['amplitude'] = 1
@@ -50,8 +72,8 @@ if __name__ == "__main__":
             cond['roivalue'] = 24
             spec.append(cond)
 
-        # TODO: mask_subjspace
-        mask_subjspace = mask2subjspace(sub, run, data_basedir, workdir, maskpath)
+        # transform mask to subject space
+        mask_subjspace = mask2subjspace_real(maskpath, anat, boldfile,mni2anat_hd5, affine_matrix, workdir)
 
         # add signal
         with_signal = add_signal_custom(noise, mask_subjspace, spec)
